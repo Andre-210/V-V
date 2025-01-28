d432f5314edec45d298c3e86de9ee75095eea1e3
andre <andre.melendez5835@gmail.com>
2025-01-27 20:23:24 -0700

    Added tests

---
+++ class_assignment_2/src/test/java/org/example/AvlTreeTest.java
@@ -0,0 +1,86 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AvlTreeTest {
+
+    @org.junit.jupiter.api.Test
+    void countStructure() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dumpRootStructure() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hybridRepOK() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void left() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void right() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findMinimum() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void bfsTraverse() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dfsTraverse() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void repOK_ConcretePost() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void repOK_StructurePost() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void repOK_Concrete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void repOK_Structure() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void repOK_Structure_CheckHeight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void repOK_Ordered() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void repOK_ElementsAreOrdered() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dumpTree() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void myMethodTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
